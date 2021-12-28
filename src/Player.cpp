8975f38206ee48f5c7ce2836177ed3656b90c192
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-12-28 23:18:14 +0100

    First player implementation

---
+++ src/Player.cpp
@@ -0,0 +1,44 @@
+#include "Player.h"
+#include "Utilities.h"
+#include "Piece.h"
+
+#include <list>
+#include <memory>
+
+using Chess::Piece;
+using Chess::Player;
+
+const std::list<std::shared_ptr<Piece>> &Player::get_available_pieces()
+{
+	return available_pieces;
+};
+
+const std::list<std::shared_ptr<Piece>> &Player::get_lost_pieces()
+{
+	return lost_pieces;
+};
+
+void Player::add_to_lost_pieces(std::shared_ptr<Piece> &piece)
+{
+	lost_pieces.push_back(piece);
+};
+
+std::shared_ptr<Piece> &Player::remove_from_lost_pieces()
+{
+	// Removing the last one in the list
+	std::shared_ptr<Piece> removed_piece = lost_pieces.back();
+
+	lost_pieces.pop_back();
+
+	return removed_piece;
+};
+
+Chess::utilities::PlayerType Player::get_type() const
+{
+	return type;
+};
+
+Chess::utilities::Color Player::get_color() const
+{
+	return color;
+};
