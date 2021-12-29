2bf34474931b36afce56e74cbef9cb2731f4b396
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-12-29 14:56:44 +0100

    Moved the getters inline

--- include/Player.h
+++ include/Player.h
@@ -17,12 +17,24 @@ namespace Chess {
             std::list<std::shared_ptr<Piece>> lost_pieces;
         
         public:
-            const std::list<std::shared_ptr<Piece>>& get_available_pieces();
-            const std::list<std::shared_ptr<Piece>>& get_lost_pieces();
+            const std::list<std::shared_ptr<Piece>> &get_available_pieces()
+            {
+                return available_pieces;
+            };
+            const std::list<std::shared_ptr<Piece>> &get_lost_pieces()
+            {
+                return lost_pieces;
+            };
             void add_to_lost_pieces(std::shared_ptr<Piece>& piece);
             std::shared_ptr<Piece>& remove_from_lost_pieces();
-            Chess::utilities::PlayerType get_type() const;
-            Chess::utilities::Color get_color() const;
+            Chess::utilities::PlayerType get_type() const
+            {
+                return type;
+            };
+            Chess::utilities::Color get_color() const
+            {
+                return color;
+            };
     };
 }
 
--- src/Player.cpp
+++ src/Player.cpp
@@ -8,16 +8,6 @@
 using Chess::Piece;
 using Chess::Player;
 
-const std::list<std::shared_ptr<Piece>> &Player::get_available_pieces()
-{
-	return available_pieces;
-};
-
-const std::list<std::shared_ptr<Piece>> &Player::get_lost_pieces()
-{
-	return lost_pieces;
-};
-
 void Player::add_to_lost_pieces(std::shared_ptr<Piece> &piece)
 {
 	lost_pieces.push_back(piece);
@@ -33,14 +23,4 @@ std::shared_ptr<Piece> &Player::remove_from_lost_pieces()
 	lost_pieces.pop_back();
 
 	return removed_piece;
-};
-
-Chess::utilities::PlayerType Player::get_type() const
-{
-	return type;
-};
-
-Chess::utilities::Color Player::get_color() const
-{
-	return color;
 };
