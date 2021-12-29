3631111f35cb7720c6023563cfb11f95d8015c34
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-12-29 15:01:52 +0100

    Change in define guards

--- include/Player.h
+++ include/Player.h
@@ -1,5 +1,5 @@
-#ifndef player_h
-#define player_h
+#ifndef PLAYER_H
+#define PLAYER_H
 
 #include "Utilities.h"
 #include "Piece.h"
@@ -38,4 +38,4 @@ namespace Chess {
     };
 }
 
-#endif
+#endif // PLAYER_H
