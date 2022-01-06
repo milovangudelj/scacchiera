f05d70f5fa1c02c6dea9b844dc108bab0fa687c9
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-06 12:10:12 +0100

    Partial testing of the player class

--- src/targets/test.cpp
+++ src/targets/test.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
 #include "Board.h"
+#include "Player.h"
+
+using Chess::Board;
+using Chess::Player;
 
 int main() {
-    Chess::Board board {"rnbqkbnr/8/8/8/8/8/8/RNBQKBNR"};
-    std::cout << board;
+    Board board{"rnbqkbnr/8/8/8/8/8/8/RNBQKBNR"};
+    std::cout << board << "\n\n";
+
+    Player player_1("white", "human", "Milo");
+    Player player_2("black", "computer");
+
+    std::cout << player_1 << "\n\n"
+              << player_2 << '\n';
     return 0;
 }
