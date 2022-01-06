829ec5d136563af2980dbf18515a3238b9bcbe7b
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-06 12:09:03 +0100

    Added constructors and operator<<

--- include/Player.h
+++ include/Player.h
@@ -6,18 +6,40 @@
 
 #include <list>
 #include <memory>
+#include <ostream>
 
 namespace Chess {
 
     class Player {
         private:
+            std::string name;
             Chess::utilities::PlayerType type;
             Chess::utilities::Color color;
             std::list<std::shared_ptr<Piece>> available_pieces;
             std::list<std::shared_ptr<Piece>> lost_pieces;
         
         public:
+            // Constructors
+
+            Player() = delete;
+
+            /// @brief Constructs a Player object with the given color, type and name.
+            /// @param _color The color of the player. Either 'white' or 'black'
+            /// @param _type The type of the player. Either 'human' or 'computer'
+            /// @param _name The name of the player.
+            Player(const std::string &_color, const std::string &_type, const std::string &_name);
+
+            /// @brief Constructs a Player object with the given color and type.
+            /// @param _color The color of the player. Either 'white' or 'black'
+            /// @param _type The type of the player. Either 'human' or 'computer'
+            Player(const std::string &_color, const std::string &_type) : Player(_color, _type, ""){};
+
+            /// @brief Constructs a 'human' Player object of the given color.
+            /// @param _color The color of the player. Either 'white' or 'black'
+            Player(const std::string &_color) : Player(_color, "human", ""){};
+
             // Simple inline getters
+
             const std::list<std::shared_ptr<Piece>> &get_available_pieces()
             {
                 return available_pieces;
@@ -36,10 +58,15 @@ namespace Chess {
             };
 
             // Management member functions
+
             void add_to_lost_pieces(std::shared_ptr<Piece> &piece);
             void add_to_available_pieces(std::shared_ptr<Piece> &piece);
             std::shared_ptr<Piece> &remove_from_lost_pieces(std::shared_ptr<Piece> &piece);
             std::shared_ptr<Piece> &remove_from_available_pieces(std::shared_ptr<Piece> &piece);
+
+            // Overloaded operators
+
+            friend std::ostream &operator<<(std::ostream &os, const Player &player);
     };
 }
 
--- src/player.cpp
+++ src/player.cpp
@@ -4,9 +4,33 @@
 
 #include <list>
 #include <memory>
+#include <ostream>
 
 using Chess::Piece;
 using Chess::Player;
+using Chess::utilities::Color;
+using Chess::utilities::PlayerType;
+
+// Constructors
+
+Player::Player(const std::string &_color, const std::string &_type, const std::string &_name)
+{
+	if (_color.compare("white") != 0 && _color.compare("black") != 0)
+	{
+		throw "Invalid player color";
+	}
+
+	if (_type.compare("human") != 0 && _type.compare("computer") != 0)
+	{
+		throw "Invalid player type";
+	}
+
+	type = _type.compare("human") == 0 ? PlayerType::human : PlayerType::computer;
+	color = _color.compare("white") == 0 ? Color::white : Color::black;
+	name = (_name.length() == 0) ? ((type == PlayerType::human) ? "Human" : "Hal") : _name;
+};
+
+// Management methods
 
 void Player::add_to_lost_pieces(std::shared_ptr<Piece> &piece)
 {
@@ -35,3 +59,14 @@ std::shared_ptr<Piece> &Player::remove_from_available_pieces(std::shared_ptr<Pie
 
 	return piece;
 };
+
+// Overloaded operators
+
+namespace Chess
+{
+	std::ostream &operator<<(std::ostream &os, const Player &player)
+	{
+		os << "Player:\n _name: " << player.name << "\n _color: " << player.color << "\n _type: " << player.type;
+		return os;
+	};
+}
