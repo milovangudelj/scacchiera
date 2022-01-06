9ea5676a82b728a25c40421ac15f3638ff261208
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-06 22:16:24 +0100

    Updated Board constructor call

--- src/targets/test.cpp
+++ src/targets/test.cpp
@@ -8,12 +8,13 @@ using Chess::Player;
 using Chess::utilities::Color;
 using Chess::utilities::PlayerType;
 
-int main() {
-    Board board{"rnbqkbnr/8/8/8/8/8/8/RNBQKBNR"};
-    std::cout << board << "\n\n";
-
+int main()
+{
     Player player_1(Color::white, PlayerType::human, "Milo");
     Player player_2(Color::black, PlayerType::computer);
+
+    Board board{"rnbqkbnr/8/8/8/8/8/8/RNBQKBNR", player_1, player_2};
+    std::cout << board << "\n\n";
 
     std::cout << player_1 << "\n\n"
               << player_2 << '\n';
