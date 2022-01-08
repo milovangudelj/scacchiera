387ac6d139d60b29f6f7edec40151e87a2e7548a
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-08 15:02:40 +0100

    Added pawns to fen string

--- src/targets/test.cpp
+++ src/targets/test.cpp
@@ -13,7 +13,7 @@ int main()
     Player player_1(Color::white, PlayerType::human, "Milo");
     Player player_2(Color::black, PlayerType::computer);
 
-    Board board{"rnbqkbnr/8/8/8/8/8/8/RNBQKBNR", player_1, player_2};
+    Board board{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", player_1, player_2};
     std::cout << board << "\n\n";
 
     std::cout << player_1 << "\n\n"
