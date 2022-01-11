8943958aefc9630b84dc77be02f0091f147f93b3
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-11 23:36:37 +0100

    Moved everything in `controller.cpp`

--- src/targets/test.cpp
+++ src/targets/test.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 
+#include "Controller.h"
 #include "Board.h"
 #include "Player.h"
 
 using Chess::Board;
+using Chess::Controller;
 using Chess::Player;
 using Chess::utilities::Color;
 using Chess::utilities::PlayerType;
 
 int main()
 {
-    Player player_1(Color::white, PlayerType::human, "Milo");
-    Player player_2(Color::black, PlayerType::computer);
+    Controller controller{};
 
-    Board board{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", player_1, player_2};
-    std::cout << board << "\n\n";
+    controller.play();
 
-    std::cout << player_1 << "\n\n"
-              << player_2 << '\n';
-    board.move(player_1, player_2, {{7, 1}, {5, 0}});
-    std::cout << board << "\n\n";
     return 0;
 }
