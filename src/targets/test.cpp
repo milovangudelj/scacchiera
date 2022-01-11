b5a6707b179a0a6c4d01a4ccacb715c93dc2a87c
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-11 23:36:37 +0100

    Moved everything in `controller.cpp`

--- src/targets/test.cpp
+++ src/targets/test.cpp
@@ -1,40 +1,22 @@
 #include <iostream>
 
+#include "Controller.h"
 #include "Board.h"
 #include "Player.h"
 
 using Chess::Board;
+using Chess::Controller;
+using Chess::Coordinate;
+using Chess::Movement;
 using Chess::Player;
 using Chess::utilities::Color;
 using Chess::utilities::PlayerType;
-using Chess::Movement;
-using Chess::Coordinate;
 
 int main()
 {
-    Player player_1(Color::white, PlayerType::human, "Milo");
-    Player player_2(Color::black, PlayerType::computer);
+    Controller controller{};
 
-    Board board{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", player_1, player_2};
-    std::cout << board << "\n\n";
+    controller.play();
 
-    /*
-    std::cout << player_1 << "\n\n"
-              << player_2 << '\n';
-    board.move(player_1, player_2, {{6, 3}, {4, 3}});
-    std::cout << board << "\n\n";
-    board.move(player_1, player_2, {{7, 3}, {5, 3}});
-    std::cout << board << "\n\n";
-    board.move(player_1, player_2, {{7, 2}, {6, 3}});
-    std::cout << board << "\n\n";
-    board.move(player_1, player_2, {{7, 1}, {5, 0}});
-    std::cout << board << "\n\n";
-    board.move(player_1, player_2, {{7, 0}, {7, 4}});
-    std::cout << board << "\n\n";
-    */
-    Coordinate start {6, 3};
-    Coordinate end {3, 3};
-    Movement movement {start, end};
-   std::cout << movement;
     return 0;
 }
