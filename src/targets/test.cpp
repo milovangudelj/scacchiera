cfd6ec8a404d25e01a1e2e682454be8cf0be7688
Yihui Zhu <zhuyihui@protonmail.com>
2022-01-12 15:45:43 +0100

    fix << of coordinate and movement

--- src/targets/test.cpp
+++ src/targets/test.cpp
@@ -7,6 +7,8 @@ using Chess::Board;
 using Chess::Player;
 using Chess::utilities::Color;
 using Chess::utilities::PlayerType;
+using Chess::Movement;
+using Chess::Coordinate;
 
 int main()
 {
@@ -16,6 +18,7 @@ int main()
     Board board{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", player_1, player_2};
     std::cout << board << "\n\n";
 
+    /*
     std::cout << player_1 << "\n\n"
               << player_2 << '\n';
     board.move(player_1, player_2, {{6, 3}, {4, 3}});
@@ -28,5 +31,10 @@ int main()
     std::cout << board << "\n\n";
     board.move(player_1, player_2, {{7, 0}, {7, 4}});
     std::cout << board << "\n\n";
+    */
+    Coordinate start {6, 3};
+    Coordinate end {3, 3};
+    Movement movement {start, end};
+   std::cout << movement;
     return 0;
 }
