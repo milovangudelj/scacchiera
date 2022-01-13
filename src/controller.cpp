bcb112015ac196ecf99b73e218b28a8d32c40baa
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-13 21:50:38 +0100

    Added `mode`,  fixed rand generation, new output

--- src/controller.cpp
+++ src/controller.cpp
@@ -17,16 +17,17 @@ static const char *RESET = "\033[0m";
 static const char *RED_FG = "\033[31m";
 static const char *BLUE_FG = "\033[34m";
 
-Controller::Controller() : white(nullptr), black(nullptr), board(nullptr)
+Controller::Controller(std::string mode) : white(nullptr), black(nullptr), board(nullptr)
 {
-	init("pc");
+	init(mode);
 }
 
 void Controller::init(const std::string &type)
 {
 	if (type.compare("pc") == 0)
 	{
-		bool white_is_human = rand() % 1 == 1;
+		srand(time(NULL));
+		bool white_is_human = rand() % 2 == 1;
 
 		white = std::make_shared<Player>(Color::white, PlayerType{white_is_human ? PlayerType::human : PlayerType::computer});
 		black = std::make_shared<Player>(Color::black, PlayerType{white_is_human ? PlayerType::computer : PlayerType::human});
@@ -108,8 +109,8 @@ void clear_errors(std::set<std::string> &errors)
 /// @brief Starts the game and goes on until either checkmate or draw occurs
 void Controller::play()
 {
-	std::shared_ptr<Chess::Player> current_player = black;
-	std::shared_ptr<Chess::Player> other_player = white;
+	std::shared_ptr<Chess::Player> current_player = white;
+	std::shared_ptr<Chess::Player> other_player = black;
 	bool check = false;
 	bool checkmate = false;
 	bool draw = false;
@@ -120,7 +121,7 @@ void Controller::play()
 
 	while (!checkmate && !draw)
 	{
-		display(current_player.get()->get_name(), checkmate, draw, check);
+		display(current_player.get(), checkmate, draw, check);
 		std::cout << ": ";
 		for (std::string e : errors)
 		{
@@ -147,16 +148,16 @@ void Controller::play()
 		case Chess::utilities::MoveResult::ok:
 			clear_errors(errors);
 			// Swap players
-			// current_player = current_player == white ? black : white;
-			// other_player = other_player == white ? black : white;
+			current_player = current_player == white ? black : white;
+			other_player = other_player == white ? black : white;
 			// Add movement to history
 			history.push_back(mvmt);
 			break;
 
 		default:
 			// Swap players
-			// current_player = current_player == white ? black : white;
-			// other_player = other_player == white ? black : white;
+			current_player = current_player == white ? black : white;
+			other_player = other_player == white ? black : white;
 			// Add movement to history
 			history.push_back(mvmt);
 			break;
@@ -166,18 +167,20 @@ void Controller::play()
 		checkmate = board.get()->is_checkmate(*current_player.get(), *other_player.get());
 		draw = board.get()->is_draw(*current_player.get(), *other_player.get());
 	}
-	display(current_player.get()->get_name(), checkmate, draw, check);
+	display(current_player.get(), checkmate, draw, check);
 
 	std::cout << "Game Over...\n\n";
 	export_game(); // Right now it only prints the history to the terminal
 }
 
-void Controller::display(const std::string &whose_turn, bool is_checkmate, bool is_draw, bool is_check)
+void Controller::display(const Chess::Player *current_player, bool is_checkmate, bool is_draw, bool is_check)
 {
 	const char *checkmate = is_checkmate ? "true" : "false";
 	const char *draw = is_draw ? "true" : "false";
+	const char *name = current_player->get_name().c_str();
+	const char *color = current_player->get_color() == Chess::utilities::Color::white ? "█" : "░";
 
-	printf("%sNow playing:%s %s	", BRIGHT, RESET, whose_turn.c_str());
+	printf("%sNow playing:%s %s %s	", BRIGHT, RESET, name, color);
 	printf("%sCheckmate:%s %s	", BRIGHT, RESET, checkmate);
 	printf("%sDraw:%s %s\n\n", BRIGHT, RESET, draw);
 
