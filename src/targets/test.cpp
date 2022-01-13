8302e718ea8e4a904c9a4aea23178585c8f9c56c
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-13 21:51:44 +0100

    Removed old tests, added argument parsing

--- src/targets/test.cpp
+++ src/targets/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 #include "Controller.h"
 #include "Board.h"
@@ -12,9 +13,22 @@ using Chess::Player;
 using Chess::utilities::Color;
 using Chess::utilities::PlayerType;
 
-int main()
+int main(int argc, char *argv[])
 {
-    Controller controller{};
+    const char *BRIGHT = "\033[1m";
+    const char *RESET = "\033[0m";
+
+    if (argc > 1 && strcmp(argv[1], "pc") != 0 && strcmp(argv[1], "cc") != 0)
+    {
+        std::cout << "Se fornito, il primo argomento deve essere uno tra i seguenti:\n"
+                  << BRIGHT << "   pc" << RESET << "   Player vs Computer\n"
+                  << BRIGHT << "   cc" << RESET << "   Computer vs Computer\n";
+        return 0;
+    }
+
+    std::string mode = argc > 1 ? argv[1] : "pc";
+
+    Controller controller{mode};
 
     controller.play();
 
