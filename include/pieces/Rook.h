223e6c8d9435c7e9ce6a97eb9b22f40f75dca7e8
JiNT1 <tommaso.jin@studenti.unipd.it>
2022-01-13 14:46:25 +0100

    Update Rook.h

--- include/pieces/Rook.h
+++ include/pieces/Rook.h
@@ -11,9 +11,6 @@ namespace Chess
         public:
 		Rook(Coordinate coordinate, Chess::utilities::Color color, Chess::utilities::PieceType type);
  		std::list<Movement> get_pseudo_valid_movements(Board& board);
-		private:
-		Coordinate short_castling(Board& board,Chess::utilities::Color color);
-		Coordinate long_castling(Board& board,Chess::utilities::Color color);
 	};
 }
 
