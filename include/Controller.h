2c09fb6e782ddf95b554f223bc17ce9964c9a0dc
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-01-16 00:12:27 +0100

    Added base for replay

--- include/Controller.h
+++ include/Controller.h
@@ -3,6 +3,7 @@
 
 #include <list>
 #include <memory>
+#include <vector>
 
 #include "Player.h"
 #include "Movement.h"
@@ -13,7 +14,9 @@ namespace Chess
 	class Controller
 	{
 	private:
-		std::string mode;
+		std::string fen;
+		std::list<Movement> log_list;
+		bool is_replay = false;
 		std::shared_ptr<Chess::Player> white;
 		std::shared_ptr<Chess::Player> black;
 		std::list<Chess::Movement> history;
@@ -21,12 +24,15 @@ namespace Chess
 		Chess::Movement get_move();
 		void export_game();
 		void init(const std::string &type);
+		void init_replay();
 
 	public:
-		Controller(std::string mode = "pc");
+		Controller(std::string _mode, std::string _fen);
+		Controller(std::list<Chess::Movement> log_list);
 
 		void play();
-		void display(const Chess::Player *current_player, bool is_checkmate, bool is_draw, bool is_check = false);
+		std::list<std::string> replay(char out);
+		void display(std::shared_ptr<Chess::Player> current_player, bool is_checkmate, bool is_draw, bool is_check = false);
 	};
 } // namespace Chess
 
