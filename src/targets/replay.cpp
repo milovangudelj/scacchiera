cbb490dc4eb31039bf2365aa73b96742bc1e20a7
JiNT1 <tommaso.jin@studenti.unipd.it>
2022-01-18 13:25:43 +0100

    Update replay.cpp
    add method_v and method_f
    add code for printing board every 1 sec

--- src/targets/replay.cpp
+++ src/targets/replay.cpp
@@ -2,13 +2,15 @@
 #include <cstring>
 #include <fstream>
 #include <list>
+#include <thread>
+#include <chrono>
 
 #include "Movement.h"
-#include "Board.h"
 #include "Controller.h"
-#include "Player.h"
 
 std::list<Chess::Movement> get_movements(std::istream& in);
+void method_v(std::ifstream& in_f,char c);
+void method_f(std::ifstream& in_f,std::ofstream& out_f,char c);
 
 int main(int argc, char *argv[])
 {
@@ -17,20 +19,28 @@ int main(int argc, char *argv[])
 	{
 		std::cerr<<"not enough information given\n";
 	}
-	if(argc <=4 && std::strcmp(argv[1],"v") != 0 && std::strcmp(argv[1],"f") != 0)
+	if( std::strcmp(argv[1],"v") != 0 && std::strcmp(argv[1],"f") != 0)
 	{
 		std::cerr<<"ERROR : command not found\n"<<"Available commands are: \t"<<"v or f\n";
 	}
-	if(argc = 3 && std::strcmp(argv[1],"v") == 0 && argv[2] == nullptr)
+	if(std::strcmp(argv[1],"v") == 0 && argc != 3)
 	{
 		std::cerr<<"file not found\n";
 	}
-	if(argc = 4 && std::strcmp(argv[1],"f") == 0)
+	if(std::strcmp(argv[1],"f") == 0 && argc != 4)
 	{
-		if(argv[2] == nullptr || argv[3] == nullptr)
+		std::cerr<<"file not found\n";
+	}
+	if(argc > 4)
+	{
+		if(std::strcmp(argv[1],"v") == 0)
 		{
-			std::cerr<<"file not found\n";
-		}	
+			std::cerr<<"should provide only one file\n";
+		}
+		if(std::strcmp(argv[1],"f") == 0)
+		{
+			std::cerr<<"should provide two files\n";
+		}
 	}
 
 	std::ifstream log_ifile;
@@ -38,13 +48,6 @@ int main(int argc, char *argv[])
 	std::list<Chess::Movement> log_list;
 	std::list<std::string> print_list;
 	char out = (std::strcmp(argv[1],"v") == 0 ? 't' : 'f');
-	std::shared_ptr<Chess::Player> white;
-	white = std::make_shared<Chess::Player> (Chess::utilities::Color::white, Chess::utilities::PlayerType::computer);
-	std::shared_ptr<Chess::Player> black; 
-	black = std::make_shared<Chess::Player> (Chess::utilities::Color::black, Chess::utilities::PlayerType::computer);
-	std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0";
-	std::shared_ptr<Chess::Board> board;
-	board = std::make_shared<Chess::Board> (fen,white,black);
 
 	//comando v
 	if(std::strcmp(argv[1],"v") == 0)
@@ -52,42 +55,22 @@ int main(int argc, char *argv[])
 		log_ifile.open(argv[2]);
 		if(log_ifile.is_open())
 		{
-			log_list = get_movements(log_ifile);
-			//stampa a video il replay con pausa di 1s
-			//...passo a controller la lista di movement e stampa a video out=='t'...
-			//Chess::Controller controller {log_list};
-			//print_list = controller.replay(out);
-			std::cout<<*board.get()<<std::endl; //configurazione base
-			for(Chess::Movement movement : log_list)
-			{
-				board.get()->move(*white.get(),*black.get(),movement);
-				std::cout<<*board.get()<<"\n";
-			}
+			method_v(log_ifile,out);
 			log_ifile.close();
 		}else
 		{
 			throw std::invalid_argument("File has not been opened properly");
 		}
 	}
-
+	
 	//comando f
 	if(std::strcmp(argv[1],"f") == 0)
 	{
 		log_ifile.open(argv[2]);
 		log_ofile.open(argv[3]);
-		if(log_ifile.is_open())
+		if(log_ifile.is_open() && log_ofile.is_open())
 		{
-			log_list = get_movements(log_ifile);
-			//stampa su file il replay senza pausa
-			//...passo a controller la lista di movement e stampa su file out=='f'...
-			//Chess::Controller controller {log_list};
-			//print_list = controller.replay(out);
-			std::cout<<*board.get()<<std::endl; //configurazione base
-			for(Chess::Movement movement : log_list)
-			{
-				board.get()->move(*white.get(),*black.get(),movement);
-				log_ofile<<*board.get()<<"\n";
-			}
+			method_f(log_ifile,log_ofile,out);
 			log_ifile.close();
 			log_ofile.close();
 		}else
@@ -95,10 +78,11 @@ int main(int argc, char *argv[])
 			throw std::invalid_argument("File has not been opened properly");
 		}
 	}
+	
 	return 0;
 }
 
-//dal file log, estraggo e inserisco nella lista di movement
+/********************dal file log, estraggo e inserisco nella lista di movement*****************/
 std::list<Chess::Movement> get_movements(std::istream& in)
 {
 	std::string s;
@@ -111,9 +95,40 @@ std::list<Chess::Movement> get_movements(std::istream& in)
 		pos_whitespace = s.find(' ');
 		start_s = s.substr(0,pos_whitespace);
 		end_s = s.substr(pos_whitespace+1);
-		start_c = {(unsigned int)(start_s.at(1)-'0'),(unsigned int)(start_s.at(0)-'A')};
-		end_c = {(unsigned int)(end_s.at(1)-'0'),(unsigned int)(end_s.at(0)-'A')};
+		start_c = {(unsigned int)(8-(start_s.at(1)-'0')),(unsigned int)(start_s.at(0)-'A')};
+		end_c = {(unsigned int)(8-(end_s.at(1)-'0')),(unsigned int)(end_s.at(0)-'A')};
 		movements.push_back({start_c,end_c});
 	}
 	return movements;
+}
+
+/********************stampa scacchiera su terminale*****************/
+void method_v(std::ifstream& in_f, char c )
+{
+	std::list<Chess::Movement> log_list;
+	std::list<std::string> print_list;
+	log_list = get_movements(in_f);
+	//stampa a video il replay con pausa di 1s
+	Chess::Controller controller {log_list};
+	print_list = controller.replay(c);
+	for(std::string config : print_list)
+	{
+		std::cout<<config<<"\n";
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+}
+
+/********************stampo scacchiera su file*****************/
+void method_f(std::ifstream& in_f,std::ofstream& out_f,char c)
+{
+	std::list<Chess::Movement> log_list;
+	std::list<std::string> print_list;
+	log_list = get_movements(in_f);
+	//stampa su file il replay senza pausa
+	Chess::Controller controller {log_list};
+	print_list = controller.replay(c);
+	for(std::string config : print_list)
+	{
+		out_f<<config<<"\n";
+	}
 }
