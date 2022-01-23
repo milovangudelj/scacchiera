bfecfc840d66ee138457fb82bdbbf89fccb34366
JiNT1 <tommaso.jin@studenti.unipd.it>
2022-01-23 01:28:14 +0100

    Added comments and formatting pawn,rook and replay

--- src/pieces/rook.cpp
+++ src/pieces/rook.cpp
@@ -15,19 +15,27 @@ Rook::Rook(Coordinate coordinate, Color color, PieceType type) : Piece{coordinat
 	symbol = (color == Color::black) ? 'T' : 't';
 }
 
+/// @brief to get the list of possible movements
+/// @param board reference to board
+/// @returns list<Movement>
 std::list<Movement> Rook::get_pseudo_valid_movements(Board &board)
 {
 	Coordinate test_coordinate;
 	std::list<Movement> pseudo_movements;
+
+	// rook can only move horizontally or vertically, through any number of unoccupied squares
 	for (int i = 0; i < 4; i++)
 	{
 		Piece *test_piece;
 		Direction direction = static_cast<Direction>(i);
 		std::pair<int, int> offset = DirectionOffset.at(direction);
+
 		test_coordinate = this->coordinate + offset;
+
 		while (test_coordinate.is_valid())
 		{
 			test_piece = board.get_piece_at(test_coordinate);
+
 			if (test_piece == nullptr)
 			{
 				pseudo_movements.push_back({this->coordinate, test_coordinate});
@@ -35,14 +43,18 @@ std::list<Movement> Rook::get_pseudo_valid_movements(Board &board)
 			else
 			{
 				Color test_piece_color = test_piece->get_color();
+
 				if (color != test_piece_color)
 				{
 					pseudo_movements.push_back({this->coordinate, test_coordinate});
 				}
+
 				break;
 			}
+
 			test_coordinate = test_coordinate + offset;
 		}
 	}
+
 	return pseudo_movements;
 }
