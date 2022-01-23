55e2aa62e5cc54f59ad23eb035d9b43edb3386c6
Yihui Zhu <yihui.zhu@studenti.unipd.it>
2022-01-23 19:41:29 +0100

    reorder help

--- src/targets/scacchiera.cpp
+++ src/targets/scacchiera.cpp
@@ -74,10 +74,10 @@ std::pair<std::string, std::string> parse_args(int argc, char *argv[])
 		params = std::pair<std::string, std::string>(def_mode, def_fen);
 		break;
 	case 2:
-		if(strcmp(argv[1], "pc") != 0 && strcmp(argv[1], "pp") != 0 && strcmp(argv[1], "cc") != 0) {
+		if(strcmp(argv[1], "help") == 0) {
 			print_help();
 		}
-		if(strcmp(argv[1], "help") != 0) {
+		if(strcmp(argv[1], "pc") != 0 && strcmp(argv[1], "pp") != 0 && strcmp(argv[1], "cc") != 0) {
 			print_help();
 		}
 		params = std::pair<std::string, std::string>(argv[1], def_fen);
